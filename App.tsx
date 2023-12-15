fe64814ce2f091e5aac322ebe44a9dfdf1810d76
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2023-12-15 17:05:14 +0300

    Add README.md

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fuggit!</Text>
+      <Text>Fuggit! now!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project with dev series for December.
