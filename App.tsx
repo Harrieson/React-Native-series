01544400d544ce0924ba7fad447ee57fa21a44dc
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2023-12-16 20:38:27 +0300

    Custom Fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
+import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import DayListItem from './src/components/core/DayList';
+
+
+SplashScreen.preventAutoHideAsync();
+
+const days = [...Array(30)].map((val, index) => index + 1);
 
 export default function App() {
+  const [ fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold
+  });
+
+  useEffect(() => {
+    if (fontsLoaded &&  !fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Fuggit! now!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={days} contentContainerStyle={styles.content} numColumns={2} columnWrapperStyle={styles.column}
+      renderItem={({ item }) => <DayListItem day={item} />} />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,7 +41,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+ 
+  column: {
+    gap: 10,
+  },
+  content: {
+    gap: 10,
+    padding: 10,
+  }
 });
--- src/components/core/DayList.tsx
+++ src/components/core/DayList.tsx
@@ -42,5 +42,6 @@ const styles = StyleSheet.create({
     text: {
       color: '#9b4521',
       fontSize: 70,
+      fontFamily: 'AmaticBold',
     }
   });
