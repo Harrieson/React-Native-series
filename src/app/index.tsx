d5fca44e69e0ca9c7071198461ca4688bd615da2
Harrie_Mburu <40524846+Harrieson@users.noreply.github.com>
2023-12-17 13:54:56 +0300

    Separate default to Root Layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,5 @@
-import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc';
-import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import DayListItem from '../components/core/DayList';
 
@@ -11,23 +8,7 @@ SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(30)].map((val, index) => index + 1);
 
-export default function Home() {
-  const [ fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold
-  });
-
-  useEffect(() => {
-    if (fontsLoaded &&  !fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError])
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-  
+export default function Home() {  
   return (
     <View style={styles.container}>
       <FlatList data={days} contentContainerStyle={styles.content} numColumns={2} columnWrapperStyle={styles.column}
